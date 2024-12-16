3b7c562c69329a75577f9363dd68a517b5ae4f7d
iamanggi <anggipuspita452@gmail.com>
2024-12-16 17:24:18 +0700

    Menambahkan DosenDao dengan 5 operasi yaitu CRUD dan detail

---
+++ app/src/main/java/com/example/ucp2/data/dao/MataKuliahDao.kt
@@ -0,0 +1,32 @@
+package com.example.ucp2.data.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.example.ucp2.data.entity.MataKuliah
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface MataKuliahDao {
+
+    @Query("SELECT * FROM matakuliah ORDER BY namaMK ASC")
+    fun getAllMatakuliah() : Flow<List<MataKuliah>>
+
+    // menambahkan fungsi untuk mengambil data berdasarkan kode mata kuliah
+    @Query("SELECT * FROM matakuliah WHERE kode = :kode")
+    fun getMatakuliah(kode: String) : Flow<MataKuliah>
+
+    // menambahkan operasi untuk menambahkan data pada tabel mata kuliah
+    @Insert
+    suspend fun insertMatakuliah(mataKuliah: MataKuliah)
+
+    // menambahkan operasi untuk memperbarui data di tabel mata kuliah
+    @Update
+    suspend fun updateMatakuliah(mataKuliah: MataKuliah)
+
+    // menambahkan operasi untuk menhapus data di tabel mata kuliah
+    @Delete
+    suspend fun  deleteMatakuliah(mataKuliah: MataKuliah)
+}
