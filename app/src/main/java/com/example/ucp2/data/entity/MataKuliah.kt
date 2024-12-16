724e16b8e9f18e6c45a78c3598504a3aa34a82f4
iamanggi <anggipuspita452@gmail.com>
2024-12-16 16:58:30 +0700

    Menambahkan tabel Mata kuliah

--- app/src/main/java/com/example/ucp2/data/entity/MataKuliah.kt
+++ app/src/main/java/com/example/ucp2/data/entity/MataKuliah.kt
@@ -1,3 +1,16 @@
 package com.example.ucp2.data.entity
 
-data class MataKuliah()
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "matakuliah")
+data class MataKuliah(
+    @PrimaryKey
+    val kode: String,
+    val namaMK: String,
+    val SKS: String,
+    val semester: String,
+    val jenisMK: String,
+    val DosenPengampu: String
+)
