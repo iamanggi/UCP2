7ab8c4a3958ba87db8a51d748a3cb2fdb65cbd66
iamanggi <anggipuspita452@gmail.com>
2024-12-18 17:23:37 +0700

    Implementasi LocalRepositorySi untuk mengelola data Mata kuliah dari database

--- app/src/main/java/com/example/ucp2/repository/LocalRepositorySi.kt
+++ app/src/main/java/com/example/ucp2/repository/LocalRepositorySi.kt
@@ -1,11 +1,14 @@
 package com.example.ucp2.repository
 
 import com.example.ucp2.data.dao.DosenDao
+import com.example.ucp2.data.dao.MataKuliahDao
 import com.example.ucp2.data.entity.Dosen
+import com.example.ucp2.data.entity.MataKuliah
 import kotlinx.coroutines.flow.Flow
 
 class LocalRepositorySi(
-    private val dosenDao: DosenDao
+    private val dosenDao: DosenDao,
+    private val matakuliahDao: MataKuliahDao
 ):RepositorySI {
     override fun getAllDosen(): Flow<List<Dosen>> {
         return dosenDao.getAllDosen()
@@ -15,5 +18,26 @@ class LocalRepositorySi(
     }
     override suspend fun insertDosen(dosen: Dosen) {
         dosenDao.insertDosen(dosen)
+    }
+
+    //Implementasi Operasi Mata kuliah
+    override fun getAllMK(): Flow<List<MataKuliah>> {
+        return matakuliahDao.getAllMatakuliah()
+    }
+
+    override fun getMK(kode: String): Flow<MataKuliah> {
+        return matakuliahDao.getMatakuliah(kode)
+    }
+
+    override suspend fun insertMk(mataKuliah: MataKuliah) {
+        matakuliahDao.insertMatakuliah(mataKuliah)
+    }
+
+    override suspend fun updateMk(mataKuliah: MataKuliah) {
+        matakuliahDao.insertMatakuliah(mataKuliah)
+    }
+
+    override suspend fun deleteMk(mataKuliah: MataKuliah) {
+        matakuliahDao.insertMatakuliah(mataKuliah)
     }
 }
