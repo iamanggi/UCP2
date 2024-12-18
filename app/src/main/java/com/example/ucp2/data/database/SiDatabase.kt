943572f15ea0dd360938ece7ed903bb1db1afc44
iamanggi <anggipuspita452@gmail.com>
2024-12-18 17:12:12 +0700

    Menambahkan Database untuk SiDatabase dengan entitas Dosen dan MataKuliah

---
+++ app/src/main/java/com/example/ucp2/data/database/SiDatabase.kt
@@ -0,0 +1,33 @@
+package com.example.ucp2.data.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.ucp2.data.dao.DosenDao
+import com.example.ucp2.data.dao.MataKuliahDao
+import com.example.ucp2.data.entity.Dosen
+import com.example.ucp2.data.entity.MataKuliah
+
+@Database(entities = [Dosen::class, MataKuliah::class], version = 1, exportSchema = false)
+abstract class SiDatabase : RoomDatabase(){
+
+    abstract fun dosenDao() : DosenDao
+    abstract fun matakuliahDao() : MataKuliahDao
+
+    companion object{
+        @Volatile
+        private var Instance: SiDatabase? = null
+
+        fun getDatabase(context: Context): SiDatabase{
+            return (Instance ?: synchronized(this){
+                Room.databaseBuilder(
+                    context,
+                    SiDatabase::class.java,
+                    "SIDatabase"
+                )
+                    .build().also { Instance = it }
+            })
+        }
+    }
+}
