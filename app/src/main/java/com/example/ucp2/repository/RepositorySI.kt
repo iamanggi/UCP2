9ebabb018d8689cf10138278dcb1c353710b2bc6
iamanggi <anggipuspita452@gmail.com>
2024-12-18 17:17:13 +0700

    Menambahkan interface RepositorySI untuk mengelola data Mata Kuliah

--- app/src/main/java/com/example/ucp2/repository/RepositorySI.kt
+++ app/src/main/java/com/example/ucp2/repository/RepositorySI.kt
@@ -1,6 +1,7 @@
 package com.example.ucp2.repository
 
 import com.example.ucp2.data.entity.Dosen
+import com.example.ucp2.data.entity.MataKuliah
 import kotlinx.coroutines.flow.Flow
 
 interface RepositorySI {
@@ -8,4 +9,9 @@ interface RepositorySI {
     fun getAllDosen(): Flow<List<Dosen>>
     fun getDosen(nidn: String) : Flow<Dosen>
 
+    suspend fun insertMk(mataKuliah: MataKuliah)
+    fun getAllMK(): Flow<List<MataKuliah>>
+    fun getMK(kode: String) : Flow<MataKuliah>
+    suspend fun deleteMk(mataKuliah: MataKuliah)
+    suspend fun updateMk(mataKuliah: MataKuliah)
 }
