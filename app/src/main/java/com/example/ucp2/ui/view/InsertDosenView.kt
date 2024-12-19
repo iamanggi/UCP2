30e9d7c26466e97e3457695e88b1af7f663a2519
iamanggi <anggipuspita452@gmail.com>
2024-12-20 02:22:03 +0700

    Menambahkan InsertBodyDsn untuk form input dosen dan tombol simpan.

--- app/src/main/java/com/example/ucp2/ui/view/InsertDosenView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/InsertDosenView.kt
@@ -1,19 +1,25 @@
 package com.example.ucp2.ui.view
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.ucp2.ui.customwidget.TopAppBar
+import com.example.ucp2.ui.viewModel.DosenEvent
+import com.example.ucp2.ui.viewModel.DosenUiState
 import com.example.ucp2.ui.viewModel.InsertDosenViewModel
 import com.example.ucp2.ui.viewModel.PenyediaViewModel
 import kotlinx.coroutines.launch
@@ -72,5 +78,33 @@ fun InsertDosenView(
 
         }
 
+    }
+}
+
+@Composable
+fun InsertBodyDsn(
+    modifier: Modifier = Modifier,
+    onValueChange: (DosenEvent) -> Unit,
+    uiState: DosenUiState,
+    onClick:() -> Unit
+){
+    Column (
+        modifier = modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    )
+    {
+        FormDosen(
+            dosenEvent = uiState.dosenEvent,
+            onValueChange = onValueChange,
+            errorState = uiState.isEntryValid,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onClick,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Simpan")
+        }
     }
 }
