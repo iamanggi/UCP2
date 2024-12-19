e7791e72314a080b8be5a4db62d25cee4aa4bd42
iamanggi <anggipuspita452@gmail.com>
2024-12-20 02:06:34 +0700

    Menambahkan data class FormErrorState untuk validasi input form

--- app/src/main/java/com/example/ucp2/ui/viewModel/InsertDosenViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewModel/InsertDosenViewModel.kt
@@ -2,6 +2,17 @@ package com.example.ucp2.ui.viewModel
 
 import com.example.ucp2.data.entity.Dosen
 
+//berfungsi untuk validasi
+data class FormErrorState(
+    val nidn: String? = null,
+    val namaDosen: String? = null,
+    val jenisKelamin: String? = null,
+){
+    fun isValid():Boolean{
+        return nidn == null && namaDosen == null && jenisKelamin == null
+    }
+}
+
 //menyimpan input form ke dalam entity
 fun DosenEvent.toDosenEntity(): Dosen = Dosen(
     nidn = nidn,
