9d84679fa62f13ef345077b28df5af3bfcb6e8a6
iamanggi <anggipuspita452@gmail.com>
2024-12-20 01:49:02 +0700

    Menambahkan objek untuk routing halaman

---
+++ app/src/main/java/com/example/ucp2/ui/navigation/AlamatNavigasi.kt
@@ -0,0 +1,17 @@
+package com.example.ucp2.ui.navigation
+
+interface AlamatNavigasi {
+    val route: String             //untuk menyimpan routing halaman
+}
+
+object DestinasiSplashView : AlamatNavigasi{
+    override val route: String = "splash_view"
+}
+
+object DestinasiHome : AlamatNavigasi{   //object akan menjadi nama halaman/ menjadi pengenal halaman
+    override val route: String = "home"
+}
+
+object DestinasiHomeDosen : AlamatNavigasi{
+    override val route: String = "home_dosen"
+}
