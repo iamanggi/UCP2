af81e07c278169004eabfd3f690fa8d88b1e37cd
iamanggi <anggipuspita452@gmail.com>
2024-12-20 02:04:16 +0700

    Menambahkan data class DosenEvent

---
+++ app/src/main/java/com/example/ucp2/ui/viewModel/InsertDosenViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.ucp2.ui.viewModel
+
+//data class variabel yang menyimpan data input form
+data class DosenEvent(
+    val nidn: String = "",
+    val namaDosen: String = "",
+    val jenisKelamin: String = ""
+)
