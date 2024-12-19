67b781cf139d16d8361a93593a0f6d5eb6da4007
iamanggi <anggipuspita452@gmail.com>
2024-12-20 00:04:58 +0700

    Menambahkan ViewModel untuk mengelola state Dosen dan Mata Kuliah

--- app/src/main/java/com/example/ucp2/ui/viewModel/HomeViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewModel/HomeViewModel.kt
@@ -1,7 +1,77 @@
 package com.example.ucp2.ui.viewModel
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ucp2.data.entity.Dosen
 import com.example.ucp2.data.entity.MataKuliah
+import com.example.ucp2.repository.RepositorySI
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+
+class HomeTodoViewModel (
+    private val repositorySI: RepositorySI
+) : ViewModel()
+{
+    val homeDosenUiState: StateFlow<HomeUiState> = repositorySI.getAllDosen()
+        .filterNotNull()
+        .map{
+            HomeUiState(
+                listDosen = it.toList(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(HomeUiState(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                HomeUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan")
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue =
+            HomeUiState(isLoading = true)
+        )
+
+    val homeMkUiState: StateFlow<HomeUiState> = repositorySI.getAllMK()
+        .filterNotNull()
+        .map{
+            HomeUiState(
+                listMk = it.toList(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(HomeUiState(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                HomeUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan")
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue =
+            HomeUiState(isLoading = true)
+        )
+}
 
 data class HomeUiState(
     val listDosen: List<Dosen> = listOf(),
