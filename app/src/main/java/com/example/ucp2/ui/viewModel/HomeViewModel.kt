8046430034e636aaad6a010337325970c4a51437
iamanggi <anggipuspita452@gmail.com>
2024-12-20 00:06:00 +0700

    memperbarui nama class

--- app/src/main/java/com/example/ucp2/ui/viewModel/HomeViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewModel/HomeViewModel.kt
@@ -14,7 +14,7 @@ import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.stateIn
 
-class HomeTodoViewModel (
+class HomeViewModel (
     private val repositorySI: RepositorySI
 ) : ViewModel()
 {
