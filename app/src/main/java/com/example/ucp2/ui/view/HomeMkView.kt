6547becd82aad0059a8d7d48b11678b58b5f455a
iamanggi <anggipuspita452@gmail.com>
2024-12-20 03:05:28 +0700

    Menambahkan ListMatakuliah dan CardMK untuk menampilkan daftar dan detail mata kuliah

--- app/src/main/java/com/example/ucp2/ui/view/HomeMkView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/HomeMkView.kt
@@ -1,11 +1,22 @@
 package com.example.ucp2.ui.view
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -24,6 +35,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ucp2.data.entity.MataKuliah
 import com.example.ucp2.ui.customwidget.CustomBottomAppBar
 import com.example.ucp2.ui.customwidget.TopAppBar
 import com.example.ucp2.ui.viewModel.HomeMkUiState
@@ -136,6 +148,77 @@ fun BodyHomeMKView(
                 },
                 modifier = Modifier.padding(top = 100.dp)
             )
+        }
+    }
+}
+@Composable
+fun ListMatakuliah(
+    listMK: List<MataKuliah>,
+    modifier: Modifier = Modifier,
+    onClick: (String) -> Unit = { }
+){
+    LazyColumn (modifier = modifier)
+    {
+        items(
+            items = listMK,
+            itemContent = { mk ->
+                CardMK(
+                    mk = mk,
+                    onClick = {onClick(mk.kode)}
+                )
+            }
+        )
+    }
+}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CardMK(
+    mk: MataKuliah,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit = { }
+) {
+    Card (
+        onClick = onClick,
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+    ){
+        Column(modifier = Modifier.padding(12.dp)
+        )
+        {
+            Row (modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically)
+            {
+                Icon(imageVector = Icons.Filled.Person, contentDescription = "")
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(
+                    text = mk.kode,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp
+                )
+            }
+            Row (modifier = Modifier.fillMaxWidth().padding(top = 6.dp),
+                verticalAlignment = Alignment.CenterVertically)
+            {
+                Icon(imageVector = Icons.Filled.DateRange, contentDescription = "")
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(
+                    text = mk.namaMK,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp
+                )
+            }
+            Row (modifier = Modifier.fillMaxWidth().padding(top = 6.dp),
+                verticalAlignment = Alignment.CenterVertically)
+            {
+                Icon(imageVector = Icons.Filled.Home, contentDescription = "")
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(
+                    text = mk.SKS,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp
+                )
+            }
         }
     }
 }
