e842ec3669e1ca49b337856fb2fe5bfdfc192b7e
iamanggi <anggipuspita452@gmail.com>
2024-12-20 00:32:52 +0700

    Menambahkan penyediaViewModel dengan Factory untuk HomeViewModel

---
+++ app/src/main/java/com/example/ucp2/ui/viewModel/PenyediaViewModel.kt
@@ -0,0 +1,20 @@
+package com.example.ucp2.ui.viewModel
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.ucp2.SiApp
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(
+                SiApp().containerApp.repositorySI
+            )
+        }
+    }
+}
+
+fun CreationExtras.SiApp(): SiApp =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as SiApp)
