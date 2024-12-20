e15fdc94834b241a414c80b4b217468829b75d88
iamanggi <anggipuspita452@gmail.com>
2024-12-20 13:10:25 +0700

    menambahkan data kelas untuk menyimpan data input form

---
+++ app/src/main/java/com/example/ucp2/ui/viewModel/InsertMkViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.ucp2.ui.viewModel
+
+// Data class variabel yang menyimpan data input form
+data class MkEvent(
+    val kode: String = "",
+    val namaMK: String = "",
+    val SKS: String = "",
+    val semester: String = "",
+    val jenisMK: String = "",
+    val namaDosen: String = ""
+)
