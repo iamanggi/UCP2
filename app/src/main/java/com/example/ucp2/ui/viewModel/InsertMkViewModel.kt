1f8557839fc80793d3be8ade72b9b4117522463e
iamanggi <anggipuspita452@gmail.com>
2024-12-20 13:12:39 +0700

    menambahkan fungsi untuk menyimpan input kedalam entity

--- app/src/main/java/com/example/ucp2/ui/viewModel/InsertMkViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewModel/InsertMkViewModel.kt
@@ -1,5 +1,16 @@
 package com.example.ucp2.ui.viewModel
 
+import com.example.ucp2.data.entity.MataKuliah
+
+// Menyimpan input form ke dalam entity
+fun MkEvent.toMataKuliahEntity(): MataKuliah = MataKuliah(
+    kode = kode,
+    namaMK = namaMK,
+    SKS = SKS,
+    semester = semester,
+    jenisMK = jenisMK,
+    dosenPengampu = namaDosen)
+
 // Data class variabel yang menyimpan data input form
 data class MkEvent(
     val kode: String = "",
